ae6130c646804f951f7602699d7bd48ffb19d572
AlexeyKatkov <91487635+AlexeyKatkov@users.noreply.github.com>
2022-04-16 23:08:36 +0300

    Update main.cpp

--- Неделя 5-6/задача 1/main.cpp
+++ Неделя 5-6/задача 1/main.cpp
@@ -4,7 +4,12 @@ int main(){
     // Вывести на экран Н строк из нулей, причем количество нулей в каждой строке равно номеру строки. Количество строк Н вводит пользователь
     int n;
     std::cin>>n;
-    for (int i =0;i<n;i++)
-        std::cout << "0"<<std::endl;
+    std::cout<<std::endl;
+    for (int i =0;i<n;i++){
+        for (int i1 =0; i1 <=i;i1++){
+            std::cout << "0";
+        }
+        std::cout <<std::endl;
+    }
     return 0;
 }
